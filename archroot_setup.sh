377d2cdc590710ee45642eb5abe52cbdf33b935b
etienne1911 <em85@pm.me>
2021-11-07 22:30:43 +0100

    Update archroot_setup.sh

--- archroot_setup.sh
+++ archroot_setup.sh
@@ -1,6 +1,8 @@
 cd /tmp
 wget http://mir.archlinux.fr/iso/2021.11.01/archlinux-bootstrap-2021.11.01-x86_64.tar.gz
 sudo tar xzf archlinux-bootstrap-2021.11.01-x86_64.tar.gz
+cd root.x86_64/
+sudo cp /etc/resolv.conf etc
 sudo mount --bind /tmp/root.x86_64 /tmp/root.x86_64
 sudo mount -t proc /proc proc
 sudo mount --make-rslave --rbind /sys sys
