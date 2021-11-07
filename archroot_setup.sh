6199a004c51edcbd38ac18f0288657953ee2ed5a
etienne1911 <em85@pm.me>
2021-11-07 22:24:02 +0100

    Create archroot_setup.sh

---
+++ archroot_setup.sh
@@ -0,0 +1,8 @@
+cd /tmp
+wget http://mir.archlinux.fr/iso/2021.11.01/archlinux-bootstrap-2021.11.01-x86_64.tar.gz
+sudo tar xzf archlinux-bootstrap-2021.11.01-x86_64.tar.gz
+sudo mount --bind /tmp/root.x86_64 /tmp/root.x86_64
+sudo mount -t proc /proc proc
+sudo mount --make-rslave --rbind /sys sys
+sudo mount --make-rslave --rbind /dev dev
+echo "Server = http://mir.archlinux.fr/\$repo/os/\$arch" | sudo tee -a root.x86_64/etc/pacman.d/mirrorlist
