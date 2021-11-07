698c25400335fa4ac08e6de39da537a278626905
etienne1911 <em85@pm.me>
2021-11-07 23:44:51 +0100

    Update archroot_setup.sh

--- archroot_setup.sh
+++ archroot_setup.sh
@@ -1,10 +1,10 @@
 # wget http://mir.archlinux.fr/iso/2021.11.01/archlinux-bootstrap-2021.11.01-x86_64.tar.gz
 CHROOT_DIR=$1
 cd $CHROOT_DIR/root.x86_64/
-sudo cp /etc/resolv.conf etc
+#sudo cp /etc/resolv.conf etc
+#echo "Server = http://mir.archlinux.fr/\$repo/os/\$arch" | sudo tee -a etc/pacman.d/mirrorlist
 sudo mount --bind $CHROOT_DIR/root.x86_64 $CHROOT_DIR/root.x86_64
 sudo mount -t proc /proc proc
 sudo mount --make-rslave --rbind /sys sys
 sudo mount --make-rslave --rbind /dev dev
-echo "Server = http://mir.archlinux.fr/\$repo/os/\$arch" | sudo tee -a etc/pacman.d/mirrorlist
 sudo chroot $CHROOT_DIR/root.x86_64 /bin/bash
