78f981f12487a225ae482efde9d78de71c35d437
etienne1911 <em85@pm.me>
2022-03-25 22:11:12 +0100

    restore entrypoint

--- .github/actions/build-archiso/entrypoint.sh
+++ .github/actions/build-archiso/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "entry point reached"
 
-# set -o errexit -o pipefail -o nounset
+set -o errexit -o pipefail -o nounset
 
-# echo '::group::Creating builder user'
-# useradd --create-home --shell /bin/bash builder
-# passwd --delete builder
-# echo '::endgroup::'
+echo '::group::Creating builder user'
+useradd --create-home --shell /bin/bash builder
+passwd --delete builder
+echo '::endgroup::'
 
 # exec runuser builder --command 'bash -l -c /build.sh'
