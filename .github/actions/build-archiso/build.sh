d089e24a177e4e7e93a9ef3941fbac37258c1632
etienne1911 <em85@pm.me>
2022-03-25 23:09:12 +0100

    next step

--- .github/actions/build-archiso/build.sh
+++ .github/actions/build-archiso/build.sh
@@ -9,13 +9,13 @@ export HOME=/home/builder
 
 echo '::group::Setup build env'
 pwd && ls -la
-whoami
-ls /home
+whoami && ls -la /home
 # pacman -Syu
-# wget http://mir.archlinux.fr/iso/latest/archlinux-bootstrap-2022.03.01-x86_64.tar.gz
+wget http://mir.archlinux.fr/iso/latest/archlinux-bootstrap-2022.03.01-x86_64.tar.gz
 # chmod +x ./repo/archroot_setup.sh
-# mkdir /workspace/archroot
-# sudo tar xzf archlinux-bootstrap-2021.11.01-x86_64.tar.gz -C /workspace/archroot
+mkdir archroot
+sudo tar xzf archlinux-bootstrap-2021.11.01-x86_64.tar.gz -C archroot
+ls /etc/resolv.conf
 # sudo cp /etc/resolv.conf /workspace/archroot/root.x86_64/etc
 # echo "Server = http://mir.archlinux.fr/\$repo/os/\$arch" | sudo tee -a /workspace/archroot/root.x86_64/etc/pacman.d/mirrorlist
 # command: bash repo/archroot_setup.sh /workspace/archroot
