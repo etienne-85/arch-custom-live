d588f81ec795a677afd176110c7c6b7773a787d0
etienne1911 <em85@pm.me>
2022-03-26 11:24:41 +0100

    next

--- .github/actions/build-archiso/build_arch_iso.sh
+++ .github/actions/build-archiso/build_arch_iso.sh
@@ -4,12 +4,19 @@
 echo '::group::Setup build env'
 pwd && ls -la && whoami 
 # pacman -Syu
-wget -q http://mir.archlinux.fr/iso/latest/archlinux-bootstrap-2022.03.01-x86_64.tar.gz
-mkdir archroot
-sudo tar xzf archlinux-bootstrap-2022.03.01-x86_64.tar.gz -C archroot
-ls -la && ls -la archroot && cat /etc/resolv.conf
+# export ISO_URL = "http://mir.archlinux.fr/iso/latest/archlinux-bootstrap-2022.03.01-x86_64.tar.gz"
+# wget -q $ISO_URL && mkdir archroot && tar xzf archlinux-bootstrap-2022.03.01-x86_64.tar.gz -C archroot
+# ls -la && ls -la archroot
+# create custom profile from releng
+mkdir custom_profile
+cp -r /usr/share/archiso/configs/releng/ custom_profile/
+# cat /etc/resolv.conf
 # sudo cp /etc/resolv.conf /workspace/archroot/root.x86_64/etc
 # echo "Server = http://mir.archlinux.fr/\$repo/os/\$arch" | sudo tee -a /workspace/archroot/root.x86_64/etc/pacman.d/mirrorlist
 # command: bash repo/archroot_setup.sh /workspace/archroot
 # cd /tmp/local-repo
+# build image
+# mkarchiso -v -w build -o out profile/
+mkarchiso -v custom_profile/
+ls -la
 echo '::endgroup::'
